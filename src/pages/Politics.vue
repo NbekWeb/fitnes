3136f716f279a9e106e6a02060f2d3090376d940
NbekWeb <nbekdev@gmail.com>
2025-05-13 21:07:02 +0500

    add Earn page

--- src/pages/Politics.vue
+++ src/pages/Politics.vue
@@ -40,31 +40,34 @@ onUnmounted(() => {
 });
 </script>
 <template>
-  <div class="text-white px-2.5 py-4 overflow-y-hidden">
-    <div
-      class="flex w-full mb-10 overflow-x-auto h-auto overflow-y-hidden items-center gap-2"
-    >
+  <a-spin :spinning="loadingUrl.has('contrack/politics/')">
+
+    <div class="text-white px-2.5 py-4 overflow-y-hidden">
       <div
-        @click="changeSelected(i)"
-        class="text-sm min-w-max font-semibold px-4 py-1 border rounded-2xl border-dark-200 bg-dark-210"
-        v-for="(item, i) in politics"
-        :key="item.id"
-        :class="selected == i && 'border-[#eaeaea] text-dark-200 bg-white'"
+        class="flex w-full mb-10 overflow-x-auto h-auto overflow-y-hidden items-center gap-2"
       >
-        {{ item?.title }}
+        <div
+          @click="changeSelected(i)"
+          class="text-sm min-w-max font-semibold px-4 py-1 border rounded-2xl border-dark-200 bg-dark-210"
+          v-for="(item, i) in politics"
+          :key="item.id"
+          :class="selected == i && 'border-[#eaeaea] text-dark-200 bg-white'"
+        >
+          {{ item?.title }}
+        </div>
       </div>
+      <h2 class="text-xl font-semibold mb-6">
+        {{ politics?.[selected]?.title }}
+      </h2>
+      <div v-html="politics?.[selected]?.description" class=""></div>
+      <template v-if="politics?.[selected]?.files">
+        <button
+          @click="download(politics?.[selected]?.files)"
+          class="w-full bg-blue-500 rounded-3xl text-base mt-5 h-10 font-semibold text-center flex justify-center items-center"
+        >
+          Скачать файл
+        </button>
+      </template>
     </div>
-    <h2 class="text-xl font-semibold mb-6">
-      {{ politics?.[selected]?.title }}
-    </h2>
-    <div v-html="politics?.[selected]?.description" class=""></div>
-    <template v-if="politics?.[selected]?.files">
-      <button
-        @click="download(politics?.[selected]?.files)"
-        class="w-full bg-blue-500 rounded-3xl text-base mt-5 h-10 font-semibold text-center flex justify-center items-center"
-      >
-        Скачать файл
-      </button>
-    </template>
-  </div>
+  </a-spin>
 </template>
