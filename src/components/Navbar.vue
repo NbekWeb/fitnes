6f0436bcee7035816c33c9e76239c2252708d58b
NbekWeb <nbekdev@gmail.com>
2025-05-13 20:35:15 +0500

    add Earn page

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,21 +2,50 @@
 import support from "./Icons/support.vue";
 import home from "./Icons/home.vue";
 import add from "./Icons/add.vue";
-import { ref } from "vue";
+import { ref ,reactive} from "vue";
 import { useRoute, useRouter } from "vue-router";
+import close from "./Icons/close.vue";
 
+import useMedia from "@/stores/media.pinia";
+
+const mediaPinia = useMedia();
 const route = useRoute();
 const router = useRouter();
+const open = ref(false);
+
+function onClose() {
+  open.value = false;
+}
+const formState = reactive({
+  message: "",
+});
+const rules = {
+  message: [
+    {
+      required: true,
+      message: "Пожалуйста, опишите проблему",
+      trigger: "blur",
+    },
+  ],
+};
 function goTo(val = "main") {
   if (!val) {
     router.push({ name: "Dashboard" });
   } else if (val == "tg") {
-    window.open("https://t.me/arzumanov_r", "_blank");
+    open.value = true;
   } else if (val == "more") {
     router.push({ name: "More" });
   } else {
     router.push({ name: "Dashboard" });
   }
+}
+function submitHelp() {
+  helpForm.value.validate().then(() => {
+    mediaPinia.postSupport({ description: formState.message }, () => {
+      formState.message = "";
+      showFull();
+    });
+  });
 }
 </script>
 <template>
@@ -46,5 +75,39 @@ function goTo(val = "main") {
       <add class="text-2xl" />
       <span>ЕЩЁ</span>
     </div>
+    <a-drawer
+      placement="bottom"
+      :closable="false"
+      :open="open"
+      @close="onClose"
+    >
+      <div class="">
+        <h3 class="font-semibold text-xl">ПОМОЩЬ</h3>
+        <span
+          class="opacity-70 text-2xl absolute -top-6 right-1.5 p-1 rounded-full bg-dark-120"
+          @click="onClose"
+        >
+          <close />
+        </span>
+        <div>
+          <a-form :model="formState" :rules="rules" ref="helpForm">
+            <a-form-item name="message">
+              <a-textarea
+                v-model:value="formState.message"
+                :rows="5"
+                class="bg-transparent text-white"
+                placeholder="Информация о проблеме"
+              />
+            </a-form-item>
+          </a-form>
+          <button
+            class="h-10 w-full text-base font-semibold bg-blue-500 rounded-3xl"
+            @click="submitHelp"
+          >
+            ОТПРАВИТЬ ДАННЫЕ
+          </button>
+        </div>
+      </div>
+    </a-drawer>
   </div>
 </template>
